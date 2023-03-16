0bd5791198bd36bce3aa8364e5f6021450df7790
MarK22100 <fmzambrana3@gmail.com>
2023-03-16 09:54:52 -0300

    urlpropieties

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("usuarios")
-@CrossOrigin(origins = {"https://portfmz.web.app/","http://localhost:4200"})
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = {"https://portfmz.web.app","http://localhost:4200"})
 public class PersonaController {
     @Autowired IPersonaService ipersonservice;
     
