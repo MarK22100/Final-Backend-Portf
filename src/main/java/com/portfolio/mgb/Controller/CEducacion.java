ccd48e94dbaf21f1263afb3c45c87e146109e897
MarK22100 <fmzambrana3@gmail.com>
2023-03-22 18:41:33 -0300

    correccioncors

--- src/main/java/com/portfolio/mgb/Controller/CEducacion.java
+++ src/main/java/com/portfolio/mgb/Controller/CEducacion.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://portafolio-mz.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://portafolio-mz.web.app"})
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
--- src/main/java/com/portfolio/mgb/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/mgb/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://portafolio-mz.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://portafolio-mz.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
