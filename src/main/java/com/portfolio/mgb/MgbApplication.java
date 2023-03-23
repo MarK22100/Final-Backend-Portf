1c5f757227b720fd29b1c2c2eb9612910d4df417
MarK22100 <fmzambrana3@gmail.com>
2023-03-23 12:29:36 -0300

    deleteScan

--- src/main/java/com/portfolio/mgb/MgbApplication.java
+++ src/main/java/com/portfolio/mgb/MgbApplication.java
@@ -2,11 +2,9 @@ package com.portfolio.mgb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.portfolio.mgb")
 @EnableJpaRepositories
 public class MgbApplication {
 
