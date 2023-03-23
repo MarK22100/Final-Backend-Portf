391522e4df00c01f5b0cee44d73921471808e9a1
MarK22100 <fmzambrana3@gmail.com>
2023-03-23 18:44:56 -0300

    corsModif

--- src/main/java/com/portfolio/mgb/MgbApplication.java
+++ src/main/java/com/portfolio/mgb/MgbApplication.java
@@ -1,17 +1,15 @@
 package com.portfolio.mgb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.portfolio.mgb")
+@ComponentScan
 public class MgbApplication {
 
 	public static void main(String[] args) {
