c49f8e7af6438cc69185d97c36d33bfe3052a5ce
MarK22100 <fmzambrana3@gmail.com>
2023-03-23 20:49:46 -0300

    confCrs

--- src/main/java/com/portfolio/mgb/MgbApplication.java
+++ src/main/java/com/portfolio/mgb/MgbApplication.java
@@ -3,13 +3,11 @@ package com.portfolio.mgb;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@ComponentScan
 public class MgbApplication {
 
 	public static void main(String[] args) {
