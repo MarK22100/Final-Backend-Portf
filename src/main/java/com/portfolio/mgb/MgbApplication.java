f789f7f1b4000c24aa4cd739b88e11c8c76b075a
MarK22100 <fmzambrana3@gmail.com>
2023-03-24 15:59:01 -0300

    configApp

--- src/main/java/com/portfolio/mgb/MgbApplication.java
+++ src/main/java/com/portfolio/mgb/MgbApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -13,4 +14,7 @@ public class MgbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MgbApplication.class, args);
 	}
+           public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
 }
